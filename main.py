9a794645385c4e7c5de43f9fdbb543557f3973a9
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-06-14 19:12:59 -0500

    add the stolen code to update all packages
    https://stackoverflow.com/a/27071962

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from subprocess import run
 # Print the packages that need to be updated.
 # Also print that pip was obviously updated first.
 print('On top of pip, the following packages were updated:\n\n')
-print(run(['pip', 'list', '-o']))
+# print(run(['pip', 'list', '-o']))  # DEBUG
 
 # Maybe this works? (to) Upgrade pip first.
 run(['pip', 'install', '-U', 'pip'])
+
+# pip install -U `pip list --outdated | awk 'NR>2 {print $1}'`
+# Upgrade all outdated packages
+run(['pip', 'install', '-U', "`pip list --outdated | awk 'NR>2 {print $1}'`"])
