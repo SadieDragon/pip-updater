6225f755bfec23b02c7fdecce0b5ccea96fdcac8
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-06-15 21:43:15 -0500

    write out the dumb way to do this
    It works! So nyeh!

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+
+from re import split, UNICODE
 from subprocess import run
 
-# Print the packages that need to be updated.
-# Also print that pip was obviously updated first.
-print('On top of pip, the following packages were updated:\n\n')
-# print(run(['pip', 'list', '-o']))  # DEBUG
+# Grab the list of packages that need to be updated
+outdated_packages = run(['pip', 'list', '-o'], capture_output=True, text=True)
+# PEP 8 greatness
+outdated_packages = outdated_packages.stdout.strip().split('\n')
 
-# Maybe this works? (to) Upgrade pip first.
-run(['pip', 'install', '-U', 'pip'])
+# Append pip to be the first package, and remove the blank lines
+outdated_packages = ['pip'] + outdated_packages[2:]
 
-# pip install -U `pip list --outdated | awk 'NR>2 {print $1}'`
-# Upgrade all outdated packages
-run(['pip', 'install', "-U `pip list --outdated | awk 'NR>2 {print $1}'`"])
+# Go through, and announce then update the packages.
+for index, package in [*enumerate(outdated_packages)]:
+    # Pip doesn't have a version or type plonked on.
+    update_notif = 'pip was updated.'
+    # Everything else does.
+    if index != 0:
+        # Remove the excess whitespace
+        pkg_vs_type = " ".join(split("\s+", package, flags=UNICODE))
+        # And then split it (PEP8, woo!)
+        pkg_vs_type = pkg_vs_type.split(' ')
+
+        # Update the package variable to just be the package name
+        package = pkg_vs_type[0]
+
+        # The notif will be '{package} (from {vs1} to {vs2}) (type: {type})
+        update_notif = (f'{package} (from {pkg_vs_type[1]} to'
+                        f' {pkg_vs_type[2]}) (type: {pkg_vs_type[3]})')
+
+    # Upgrade the package to the latest version.
+    run(['pip', 'install', '-U', package])
---
+++ test.py
@@ -0,0 +1,5 @@
+from subprocess import run
+
+pkg = 'pip'
+
+run(['pip', 'install', '-U', pkg])
